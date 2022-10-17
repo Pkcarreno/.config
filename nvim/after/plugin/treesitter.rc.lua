8217af813afb507f42a827528d8d2e064e29697e
Pedro Carreño <pkcarrenodev@gmail.com>
2022-10-17 19:58:39 -0400

    fix(nvim): update to lua

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,22 +1,32 @@
-local status, ts = pcall(require, 'nvim-treesitter.configs')
+local status, ts = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 ts.setup {
- 	highlight = {
-		enable = true,
-		disabled = {},
-	},
-	indent = {
-		enable = true,
-		disabled = {},
-	},
-	ensure_installed = {
-		'tsx',
-		'lua',
-		'json',
-		'css'
-	},
-	autotag = {
-		enable = true,
-	}
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua",
+    "rust"
+  },
+  autotag = {
+    enable = true,
+  },
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,6 +1,5 @@
 local keymap = vim.keymap
 
--- Do not yank with X
 keymap.set('n', 'x', '"_x')
 
 -- Increment/decrement
@@ -8,22 +7,21 @@ keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb_d')
+keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+keymap.set('n', 'te', ':tabedit<cr>')
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 's<left>', '<C-w>h')
-keymap.set('', 's<up>', '<C-w>k')
-keymap.set('', 's<down>', '<C-w>j')
-keymap.set('', 's<right>', '<C-w>l')
 keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
