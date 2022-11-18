8396c61dded4cff82411e5a57cd39d66ee5f32d1
Pedro Carreño <pkcarrenodev@gmail.com>
2022-11-17 23:51:10 -0400

    fix(nvim): fix theme bufferline

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -6,7 +6,7 @@ local highlights = require('rose-pine.plugins.bufferline')
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    -- separator_style = 'slant',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
