6c8f8a7acc2887259321eda2ce3e9201c2da40c7
Pedro Carreño <pkcarrenodev@gmail.com>
2023-02-12 22:07:13 -0400

    fix(nvim): colorscheme tweaks

--- nvim/lua/pk/plugins/rose-pine.lua
+++ nvim/lua/pk/plugins/rose-pine.lua
@@ -4,11 +4,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("rose-pine").setup({
-            disable_background = true,
-            disable_float_background = true,
-            comment_italics = true,
-        })
-        vim.cmd('colorscheme rose-pine')
+      require("rose-pine").setup({
+          disable_background = true,
+          disable_float_background = true,
+          comment_italics = true,
+      })
+      vim.cmd('colorscheme rose-pine')
     end
 }
