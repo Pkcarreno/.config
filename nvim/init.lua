eecf88512df778a5eca7e63081ce253c7786fd41
Pedro Carreño <pkcarrenodev@gmail.com>
2023-02-16 09:29:55 -0400

    feat(nvim): connect the clipboard between Windows and WSL2

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,11 @@ require('pk.initPlug')
 local has = vim.fn.has
 -- local is_mac = has "macunix"
 local is_win = has "win32"
+local is_wsl = has "wsl"
 
 if is_win then
   require('pk.windows')
+end
+if is_wsl then
+  require('pk.wsl')
 end
