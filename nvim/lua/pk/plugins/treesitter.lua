298edd6a297451aa5e2dadbe3568b449e5f67f3e
Pedro Carreño <pkcarrenodev@gmail.com>
2023-04-07 18:32:37 -0400

    fix(nvim): duplicate config

--- nvim/lua/pk/plugins/treesitter.lua
+++ nvim/lua/pk/plugins/treesitter.lua
@@ -1,41 +1,39 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        highlight = {
-            enable = true,
-            disable = {},
-        },
-        indent = {
-            enable = true,
-            disable = {},
-        },
-        ensure_installed = {
-            "markdown",
-            "markdown_inline",
-            "tsx",
-            "toml",
-            "fish",
-            "php",
-            "json",
-            "yaml",
-            "css",
-            "html",
-            "lua",
-            "rust",
-            "astro"
-        },
-        autotag = {
-            enable = true,
-        },
+  'nvim-treesitter/nvim-treesitter',
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    highlight = {
+      enable = true,
+      disable = {},
     },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+    indent = {
+      enable = true,
+      disable = {},
+    },
+    ensure_installed = {
+      "markdown",
+      "markdown_inline",
+      "tsx",
+      "toml",
+      "fish",
+      "php",
+      "json",
+      "yaml",
+      "css",
+      "html",
+      "lua",
+      "rust",
+      "astro"
+    },
+    autotag = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
 
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-        parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-        parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-        parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-    end
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+  end
 }
