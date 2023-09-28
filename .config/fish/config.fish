f8c0901685e0ab5ceb66c297f02188894845ac31
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-09-28 17:06:24 -0400

    refactor: structure change

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,9 @@ set -gx TERM xterm-256color
 # theme
 set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
+set -g theme_display_user no
+set -g theme_hide_hostname yes 
+set -g theme_hostname no
 
 # aliases
 alias ls "ls -p -G"
