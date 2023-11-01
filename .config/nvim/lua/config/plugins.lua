a18687af54d1c5fca4f8f72c67c1905098635855
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-11-01 17:44:38 -0400

    feat(nvim): add notify

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -132,6 +132,30 @@ return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+    end,
+    init = function()
+      local banned_messages = {
+        "No information available",
+        "LSP[tsserver] Inlay Hints request failed. Requires TypeScript 4.4+.",
+        "LSP[tsserver] Inlay Hints request failed. File not opened in the editor.",
+      }
+      vim.notify = function(msg, ...)
+        for _, banned in ipairs(banned_messages) do
+          if msg == banned then
+            return
+          end
+        end
+        return require("notify")(msg, ...)
+      end
+    end,
+  },
+
   -- Fuzzy Finder (files, lsp, etc)
   {
     'nvim-telescope/telescope.nvim',
