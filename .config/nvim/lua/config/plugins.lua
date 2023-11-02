dee377bda4d600849546b21399fe466424dc1cb2
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-11-02 19:05:22 -0400

    feat(nvim): add diffview

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -452,4 +452,17 @@ return {
       { "<Leader>ghu", desc = "undo stage" },
     }
   },
+  {
+    "sindrets/diffview.nvim",
+    lazy = true,
+    enabled = true,
+    event = "BufRead",
+    config = function()
+      require("plugins.git.diffview")
+    end,
+    keys = {
+      { "<Leader>gd", "<cmd>lua require('plugins.git.diffview').toggle_file_history()<CR>", desc = "diff file" },
+      { "<Leader>gS", "<cmd>lua require('plugins.git.diffview').toggle_status()<CR>",       desc = "status" }
+    },
+  },
 }
