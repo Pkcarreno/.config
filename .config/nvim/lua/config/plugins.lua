7babb3d007dd8e06c23437f4d465e1110c6ad299
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-11-01 20:18:25 -0400

    feat(nvim): add tailwind addons

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -153,6 +153,44 @@ return {
       require("plugins.autopairs")
     end,
   },
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("plugins.colorizer")
+    end,
+  },
+  {
+    "js-everts/cmp-tailwind-colors",
+    config = true,
+  },
+  {
+    "razak17/tailwind-fold.nvim",
+    opts = {
+      min_chars = 50,
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = { "html", "svelte", "astro", "vue", "typescriptreact" },
+  },
+  {
+    "MaximilianLloyd/tw-values.nvim",
+    keys = {
+      { "<Leader>cv", "<CMD>TWValues<CR>", desc = "Tailwind CSS values" },
+    },
+    opts = {
+      border =  "rounded", -- Valid window border style,
+      show_unknown_classes = true                   -- Shows the unknown classes popup
+    }
+  },
+  {
+    "laytan/tailwind-sorter.nvim",
+    cmd = {
+      "TailwindSort",
+      "TailwindSortOnSaveToggle"
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm i && npm run build",
+    config = true,
+  },
 
   -- Useful plugin to show you pending keybinds.
   {
@@ -218,12 +256,6 @@ return {
     end,
   },
  
-  {
-    "NvChad/nvim-colorizer.lua",
-    config = function()
-      require("plugins.colorizer")
-    end,
-  },
 
   {
     -- Set lualine as statusline
