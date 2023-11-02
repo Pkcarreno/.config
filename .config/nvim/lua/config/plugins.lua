be13949dff900af91c36aae699b3b919d4dca95e
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-11-02 17:19:35 -0400

    feat(nvim): add spider

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -366,6 +366,24 @@ return {
       require("plugins.indent")
     end,
   },
+  {
+    "chrisgrieser/nvim-spider",
+    cond = true,
+    lazy = true,
+    keys = { "w", "e", "b", "ge" },
+    config = function()
+      vim.keymap.set({ "n", "o", "x" }, "W", "w", { desc = "Normal w" })
+      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "ge",
+        "<cmd>lua require('spider').motion('ge')<CR>",
+        { desc = "Spider-ge" }
+      )
+    end,
+  },
 
   -- Snippets & Language & Syntax
   'tpope/vim-sleuth',
