4b90ec9e6635c10df1151efd73656bbdc5d7b7b4
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-11-02 16:17:59 -0400

    feat(nvim): improve autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,23 @@
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost",
+  { callback = function() vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 100 }) end })
+-- Disable diagnostics in node_modules (0 is current buffer only)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+-- Enable spell checking for certain file types
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.tex" },
+  command = "setlocal spell" })
+-- Show `` in specific files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.json" },
+  command = "setlocal conceallevel=0" })
+
+-- Attach specific keybindings in which-key for specific filetypes
+local present, _ = pcall(require, "which-key")
+if not present then return end
+local _, pwk = pcall(require, "plugins.which-key")
+
+vim.api.nvim_create_autocmd("BufEnter", { pattern = "*.md",
+  callback = function() pwk.attach_markdown(0) end })
+vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.ts", "*.tsx" },
+  callback = function() pwk.attach_typescript(0) end })
+vim.api.nvim_create_autocmd("BufEnter", { pattern = { "package.json" },
+  callback = function() pwk.attach_npm(0) end })
