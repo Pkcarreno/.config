b045c5d183dd49f5507f21c617d444df4b1526be
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-11-16 01:05:57 -0400

    feat(nvim): add bracketed

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -23,4 +23,20 @@ return {
     },
     opts = {},
   },
+
+  -- Go forward/backward with square brackets
+  {
+    "echasnovski/mini.bracketed",
+    event = "BufReadPost",
+    config = function()
+      local bracketed = require("mini.bracketed")
+      bracketed.setup({
+        file = { suffix = "" },
+        window = { suffix = "" },
+        quickfix = { suffix = "" },
+        yank = { suffix = "" },
+        treesitter = { suffix = "n" },
+      })
+    end,
+  },
 }
