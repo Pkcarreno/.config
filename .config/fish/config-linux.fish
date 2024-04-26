c89d13221dd0eda13958761402423db7bc1f7ee6
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-04-26 15:00:23 -0400

    feat(fish): add brew support

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -6,3 +6,8 @@ end
 if type -q tmux
     alias ide "~/.scripts/ide"
 end
+
+if type -q /home/linuxbrew/.linuxbrew/bin/brew
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    set -Ux HOMEBREW_NO_AUTO_UPDATE 1
+end
