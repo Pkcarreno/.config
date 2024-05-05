7ff6b66f0debcfb4e603abe0523d2d949ce82a06
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-05-05 15:51:06 -0400

    feat(powershell): update config

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,41 +1,228 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
+# resolve paths
 $env:XDG_CONFIG_HOME = "$HOME/.config"
-
-Import-Module posh-git
-$omp_config = Join-Path $PSScriptRoot "pedro.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
-# PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
-
-# Terminal-Icons
-Import-Module -Name Terminal-Icons
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Import Modules and External Profiles
+# Ensure Terminal-Icons module is installed before importing
+if (-not (Get-Module -ListAvailable -Name Terminal-Icons))
+{
+  Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
+}
+Import-Module -Name Terminal-Icons
 
 # sfsu
 Invoke-Expression (&sfsu hook)
 
-# Alias
-Set-Alias -Name vim -Value nvim
-Set-Alias ll ls
-Set-Alias g git
-Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+# Admin Check and Prompt Customization
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+function prompt
+{
+  if ($isAdmin)
+  { "[" + (Get-Location) + "] # " 
+  } else
+  { "[" + (Get-Location) + "] $ " 
+  }
+}
+$adminSuffix = if ($isAdmin)
+{ " [ADMIN]" 
+} else
+{ "" 
+}
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
 
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+# Utility Functions
+function Test-CommandExists
+{
+  param($command)
+  $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
+  return $exists
+}
+
+# Editor Configuration
+$EDITOR = if (Test-CommandExists nvim)
+{ 'nvim' 
+} elseif (Test-CommandExists code)
+{ 'code' 
+} else
+{ 'notepad' 
+}
+Set-Alias -Name vim -Value $EDITOR
+
+function touch($file)
+{ "" | Out-File $file -Encoding ASCII 
+}
+
+# Network Utilities
+function Get-PubIP
+{ (Invoke-WebRequest http://ifconfig.me/ip).Content 
+}
+
+# System Utilities
+function uptime
+{
+  if ($PSVersionTable.PSVersion.Major -eq 5)
+  {
+    Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+  } else
+  {
+    net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
+  }
+}
+
+function reload-profile
+{
+  & $profile
+}
+
+function unzip ($file)
+{
+  Write-Output("Extracting", $file, "to", $pwd)
+  $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
+  Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+function grep($regex, $dir)
+{
+  if ( $dir )
+  {
+    Get-ChildItem $dir | select-string $regex
+    return
+  }
+  $input | select-string $regex
+}
+
+function df
+{
+  get-volume
+}
+
+function which($name)
+{
+  Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function export($name, $value)
+{
+  set-item -force -path "env:$name" -value $value;
+}
+
+function pkill($name)
+{
+  Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+
+function pgrep($name)
+{
+  Get-Process $name
+}
+
+function head
+{
+  param($Path, $n = 10)
+  Get-Content $Path -Head $n
+}
+
+function tail
+{
+  param($Path, $n = 10)
+  Get-Content $Path -Tail $n
+}
+
+# Quick File Creation
+function nf
+{ param($name) New-Item -ItemType "file" -Path . -Name $name 
+}
+
+# Directory Management
+function mkcd
+{ param($dir) mkdir $dir -Force; Set-Location $dir 
+}
+
+### Quality of Life Aliases
+
+# Navigation Shortcuts
+function docs
+{ Set-Location -Path $HOME\Documents 
+}
+
+function dtop
+{ Set-Location -Path $HOME\Desktop 
+}
+
+# Enhanced Listing
+function ll
+{ Get-ChildItem -Path . -Force | Format-Table -AutoSize 
+}
+function la
+{ Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize 
+}
+
+# Git Shortcuts
+Set-Alias g git
+
+# Quick Access to System Information
+function sysinfo
+{ Get-ComputerInfo 
+}
+
+# Networking Utilities
+function flushdns
+{ Clear-DnsClientCache 
+}
+
+# Clipboard Utilities
+function cpy
+{ Set-Clipboard $args[0] 
+}
+
+function pst
+{ Get-Clipboard 
+}
+
+
+# Enhanced PowerShell Experience
+Set-PSReadLineOption -Colors @{
+  Command = 'Yellow'
+  Parameter = 'Green'
+  String = 'DarkCyan'
+}
+
+## Final Line to set prompt
+if (-not (Get-Module -ListAvailable -Name pure-pwsh))
+{
+  Install-Module -Name pure-pwsh -Scope CurrentUser -Force -SkipPublisherCheck
+}
+Import-Module -Name pure-pwsh
+
+$pure.PwdFormatter = {
+  (
+    ((Split-Path $pwd).Replace($HOME, '~').Split($pwd.Provider.ItemSeparator) |% {$_[0]}) +
+    (Split-Path -Leaf $pwd)
+  ) -join '/'
+}
+
+if (Get-Command zoxide -ErrorAction SilentlyContinue)
+{
+  Invoke-Expression (& { (zoxide init powershell | Out-String) })
+} else
+{
+  if (Get-Command scoop -ErrorAction SilentlyContinue)
+  {
+    Write-Host "zoxide command not found. Attempting to install via scoop..."
+    try
+    {
+      Invoke-Expression "scoop install zoxide"
+      Write-Host "zoxide installed successfully. Initializing..."
+      Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    } catch
+    {
+      Write-Error "Failed to install zoxide. Error: $_"
+    }
+  } else
+  {
+    Write-Error "Install scoop in order to install zoxide."
+  }
 }
