e4f903439cbe5a00b6e3845dcb5b6b5f99ba14cf
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-05-21 15:38:57 -0400

    feat(fish): fzf

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -11,3 +11,7 @@ if type -q /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     set -Ux HOMEBREW_NO_AUTO_UPDATE 1
 end
+
+# Fzf
+set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
+set -g FZF_LEGACY_KEYBINDINGS 0
